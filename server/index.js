6f02767c3fa3e5a9953ba73933e067bdffb62134
Ibrahim Yakubu <ibrahimy437@gmail.com>
2025-12-17 13:21:29 +0100

    feat: initialize server with Express, dotenv, and a reviews API endpoint.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import reviewsRouter from './routes/reviews.js';
+
+dotenv.config();
 
 const app = express();
 const PORT = 3001;
