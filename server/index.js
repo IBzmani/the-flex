81a94dd1df38050c5c3b9e64e17171c2ccf538da
Ibrahim Yakubu <ibrahimy437@gmail.com>
2025-12-17 15:35:37 +0100

    refactor: configure for deployment on Render!

--- server/index.js
+++ server/index.js
@@ -6,13 +6,22 @@ import reviewsRouter from './routes/reviews.js';
 dotenv.config();
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 
 // Mount Routes
 app.use('/api/reviews', reviewsRouter);
+
+// Serve static files in production
+const distPath = new URL('../dist', import.meta.url).pathname;
+app.use(express.static(distPath));
+
+// Catch-all for client-side routing
+app.get('*', (req, res) => {
+  res.sendFile(distPath + '/index.html');
+});
 
 app.listen(PORT, () => {
   console.log(`API Server running on http://localhost:${PORT}`);
